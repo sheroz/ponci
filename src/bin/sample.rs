ecddc10b463383cf98885e2ce63d4efb31f4be26
Sheroz <sheroz.khaydarov@gmail.com>
2023-07-19 02:07:19 +0300

    added log levels

--- src/bin/sample.rs
+++ src/bin/sample.rs
@@ -1,12 +1,25 @@
-use log::{trace, debug, warn, info, error};
+use log;
 
 fn main() {
     println!("Poncu!");
 
+    std::env::set_var("RUST_LOG", "debug");
+
     env_logger::init();
-    trace!("log message in {} level", "TRACE");
-    debug!("log message in {} level", "DEBUG");
-    warn!("log message in {} level", "WARN");
-    info!("log message in {} level", "INFO");
-    error!("log message in {} level", "ERROR");
+
+    // log::set_max_level(log::LevelFilter::Debug);
+
+    println!("Log level: {}", log::max_level());
+
+    if log::log_enabled!(log::Level::Trace) {
+        log::trace!("log message in {} level", "TRACE");
+    }
+
+    if log::log_enabled!(log::Level::Debug) {
+        log::debug!("log message in {} level", "DEBUG");
+    }
+
+    log::warn!("log message in {} level", "WARN");
+    log::info!("log message in {} level", "INFO");
+    log::error!("log message in {} level", "ERROR");
 }
